9a791427cb1fbd65e8979f9a20644d1644e41924
maple0716 <121430876+maple0716@users.noreply.github.com>
2023-05-18 22:14:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ function main() {
     });
 
     const geom = new THREE.BoxGeometry(20, 20, 20);
+    const loader = new FontLoader();
+
+    loader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function (font) {
+
+        const geometry = new TextGeometry('Hello three.js!', {
+            font: font,
+            size: 80,
+            height: 5,
+            curveSegments: 12,
+            bevelEnabled: true,
+            bevelThickness: 10,
+            bevelSize: 8,
+            bevelSegments: 5
+        });
+    });
 
     const arjs = new THREEx.LocationBased(scene, camera);
 
@@ -108,13 +123,16 @@ function main() {
         const material2 = new THREE.MeshBasicMaterial({ color: 0xffff00 });
         const material3 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
         const material4 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const textMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
         arjs.add(new THREE.Mesh(geom, material), longitude, latitude + 0.001); // slightly north
         arjs.add(new THREE.Mesh(geom, material2), longitude, latitude - 0.001); // slightly south
         arjs.add(new THREE.Mesh(geom, material3), longitude - 0.001, latitude); // slightly west
         arjs.add(new THREE.Mesh(geom, material4), longitude + 0.001, latitude); // slightly east
+        arjs.add(new THREE.Mesh(textGeometry, textMaterial), longitude + 0.001,latitude); // slightly east 
     }
 
     requestAnimationFrame(render);
 }
 
 main();
+
